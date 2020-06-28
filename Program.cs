803e36fe7f4bfd4c3bcf0cae0e2619b1dc04af11
Novurran <postwarpoem166@gmail.com>
2020-06-28 09:49:43 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace FileCreator
+{
+    // NOTE: the created files will appear in the bin/Debug folders
+
+    class FileCreator {
+        string pathToFolder = Directory.GetCurrentDirectory();
+        string folderName = "Content";
+        // Creates folder
+        public FileCreator() {
+            if (!Directory.Exists(pathToFolder + "/" + folderName)) {
+                Directory.CreateDirectory(pathToFolder + "/" + folderName);
+            }
+        }
+
+        public void createFile(string fileName, string type, string textInsert) {
+            // Creates File
+            string file = fileName + type;
+            if (!Directory.Exists(pathToFolder + "/" + folderName + "/" + file)) {
+                try { 
+                    TextWriter writer = new StreamWriter(pathToFolder+"/"+folderName+"/"+file,true);
+
+                    writer.Write(textInsert);
+                    writer.Flush();
+                    writer.Close();
+                }
+                catch (Exception e) { Console.WriteLine(e); }
+            }
+        }
+    }
+
+    class Program {
+
+        static FileCreator fileCreator;
+
+        public static void Main(string[] args) {
+            fileCreator = new FileCreator();
+            fileCreator.createFile("test", ".txt", "test Text");
+        }
+    }
+}
